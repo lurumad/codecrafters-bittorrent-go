3290a013ea0b052677223295ad3aeb254c1b486a
lurumad <luisruizpavon@gmail.com>
2024-04-05 15:55:05 +0200

    pass 3rd stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,38 +11,76 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
+var (
+	ErrBencodeInteger = errors.New("invalid bencode integer")
+	ErrBencodeString  = errors.New("invalid bencode string")
+	ErrBencodeList    = errors.New("invalid bencode list")
+)
+
+type BencodeType = interface{}
+
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
+func decodeBencode(bencodedString string) (BencodeType, int, error) {
 	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		endIndex := strings.Index(bencodedString, "e")
-		integer, err := strconv.Atoi(bencodedString[1:endIndex])
-		if err != nil {
-			return "", err
-		}
-		return integer, nil
+		return decodeString(bencodedString)
+	} else if bencodedString[0] == 'i' {
+		return decodeInteger(bencodedString)
+	} else if bencodedString[0] == 'l' {
+		return decodeList(bencodedString)
 	} else {
-		return "", fmt.Errorf("only strings are supported at the moment")
+		return "", 0, fmt.Errorf("only strings are supported at the moment")
 	}
+}
+
+func decodeString(bencodedString string) (string, int, error) {
+	firstColonIndex := strings.Index(bencodedString, ":")
+	if firstColonIndex == -1 {
+		return "", 0, ErrBencodeString
+	}
+
+	lengthStr := bencodedString[:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", 0, err
+	}
+
+	end := firstColonIndex + 1 + length
+	return bencodedString[firstColonIndex+1 : end], end, nil
+}
+
+func decodeInteger(bencodedString string) (int, int, error) {
+	firsteIndex := strings.Index(bencodedString, "e")
+	if firsteIndex == -1 {
+		return 0, 0, ErrBencodeInteger
+	}
+
+	integer, err := strconv.Atoi(bencodedString[1:firsteIndex])
+	if err != nil {
+		return 0, 0, err
+	}
+	return integer, firsteIndex + 1, nil
+}
+
+func decodeList(bencodedString string) (BencodeType, int, error) {
+	list := make([]BencodeType, 0)
+	processedBencodedString := bencodedString[1:]
+	for ok := true; ok; ok = len(processedBencodedString) > 1 {
+		value, end, err := decodeBencode(processedBencodedString)
+		if err != nil {
+			return make([]BencodeType, 0), 0, err
+		}
+		list = append(list, value)
+		processedBencodedString = processedBencodedString[end:]
+	}
+
+	if processedBencodedString == "" || processedBencodedString[0] != 'e' {
+		return make([]BencodeType, 0), 0, ErrBencodeList
+	}
+
+	return list, len(bencodedString), nil
 }
 
 func main() {
@@ -50,7 +89,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, _, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
---
+++ cmd/mybittorrent/main_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestErrBencodeString(t *testing.T) {
+	bencodedString := "5hello"
+
+	got, end, err := decodeBencode(bencodedString)
+
+	if !errors.Is(err, ErrBencodeString) {
+		t.Errorf("expected ErrBencodeString - got: %v", err)
+	}
+
+	if got != "" {
+		t.Errorf("error result should be empty - got: %v", got)
+	}
+
+	if end > 0 {
+		t.Errorf("error end of string should be 0 - got %d", end)
+	}
+}
+
+func TestErrDecodeBencodeInteger(t *testing.T) {
+	bencodedString := "i52"
+
+	got, end, err := decodeBencode(bencodedString)
+
+	if !errors.Is(err, ErrBencodeInteger) {
+		t.Errorf("expected ErrBencodeInteger - got: %v", err)
+	}
+
+	if got != 0 {
+		t.Errorf("error result should be 0 - got: %v", got)
+	}
+
+	if end > 0 {
+		t.Errorf("error end should be 0 - got %d", end)
+	}
+}
+
+func TestDecodeBencode(t *testing.T) {
+	type testCase struct {
+		bencoded string
+		want     BencodeType
+		end      int
+	}
+
+	for _, tc := range []testCase{
+		{bencoded: "5:hello", want: "hello"},
+		{bencoded: "i52e", want: 52},
+		{bencoded: "l5:helloi52ee", want: []interface{}{"hello", 52}},
+	} {
+		got, end, err := decodeBencode(tc.bencoded)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got == "" {
+			t.Errorf("%v error result should be empty - got: %v", tc.bencoded, got)
+		}
+
+		if !equals(got, tc.want) {
+			t.Errorf("%v bad result - want %v, got %v", tc.bencoded, tc.want, got)
+		}
+
+		if end != len(tc.bencoded) {
+			t.Errorf("%v bad end - want %v, got %v", tc.bencoded, len(tc.bencoded), end)
+		}
+	}
+}
+
+func equals(a, b interface{}) bool {
+	if reflect.TypeOf(a) != reflect.TypeOf(b) {
+		return false
+	}
+
+	switch a := a.(type) {
+	case string, int:
+		return a == b
+	case []interface{}:
+		b := b.([]interface{})
+		if len(a) != len(b) {
+			return false
+		}
+		for index, item := range a {
+			if item != b[index] {
+				return false
+			}
+		}
+		return true
+	default:
+		return true
+	}
+}
